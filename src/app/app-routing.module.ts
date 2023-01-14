4673c474a2a1b55df2f0fc4c90e4fa0d933ec13e
Meecore4 <[alicia.critelli4@gmail.com](mailto:alicia.critelli4@gmail.com)>
2023-01-15 00:34:26 +0100

    2nd commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 
 // import { FormsModule } from '@angular/forms';
 import { FormulaireComponent } from './formulaire/formulaire.component';
-
+import { HeaderComponent } from './header/header.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
 
+  
+  {path: 'header', component: HeaderComponent},
+  
+  {path: 'page1', component: Page1Component},
   {path: 'formulaire', component: FormulaireComponent },
-
+  {path: 'page2', component: Page2Component},
+  {path: 'page3', component: Page3Component},
+  {path: 'page4', component: Page4Component},
+  
+  {path: 'footer', component: FooterComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,25 @@ import { AppComponent } from './app.component';
 
 
 // fait par alicia//
+// import { FormulaireComponent } from './formulaire/formulaire.component';
 import { HeaderComponent } from './header/header.component';
-import { FormulaireComponent } from './formulaire/formulaire.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
 import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    // FormulaireComponent,
     HeaderComponent,
-    FormulaireComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    Page4Component,
     FooterComponent,
-    
   ],
   
   imports: [
